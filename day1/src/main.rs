501744fd9782b252aa4383be2fbbe50786e4ded5
LouisIsbister <louis.isbister@gmail.com>
2024-12-02 21:29:07 +1300

    Updated day 1 because why not

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,20 +1,20 @@
 fn main() {
     let file = std::fs::read_to_string("input.txt").expect("Cannot read file!");
-    let (l1, l2) = compute_lists(&file);
+    let (mut l1, mut l2) = compute_lists(&file);
 
-    let res1 = distance(l1.clone(), l2.clone());
+    let res1 = distance(&mut l1, &mut l2);
     assert_eq!(2086478, res1);
 
-    let res2 = similarity_score(l1.clone(), l2.clone());
+    let res2 = similarity_score(&l1, &l2);
     assert_eq!(24941624, res2);
 }
 
 ///
 /// Task 1
 /// 
-fn distance(mut l1: Vec<i64>, mut l2: Vec<i64>) -> i64 {
-    l1.sort_by(|a, b| a.cmp(b));
-    l2.sort_by(|a, b| a.cmp(b));
+fn distance(l1: &mut Vec<i64>, l2: &mut Vec<i64>) -> i64 {
+    (*l1).sort_by(|a, b| a.cmp(b));
+    (*l2).sort_by(|a, b| a.cmp(b));
     l1.iter().zip(l2.iter())
             .map(|(a, b)| (a - b).abs())
             .sum()
@@ -23,7 +23,7 @@ fn distance(mut l1: Vec<i64>, mut l2: Vec<i64>) -> i64 {
 ///
 /// Task 2
 /// 
-fn similarity_score(l1: Vec<i64>, l2: Vec<i64>) -> i64 {
+fn similarity_score(l1: &Vec<i64>, l2: &Vec<i64>) -> i64 {
     l1.iter().fold(
         0i64, 
         |acc, x| acc + *x * (l2.iter().filter(|y| *x == **y).count() as i64)
@@ -39,11 +39,10 @@ fn compute_lists(file: &String) -> (Vec<i64>, Vec<i64>) {
     let mut l2: Vec<i64> = Vec::new();
     
     for line in file.lines() {
-        let res: Vec<i64> = line.split("   ")
-                .map(|val| val.parse().expect("Not an int!"))
-                .collect();
-        l1.push(*res.get(0).expect("Cannot get first element!"));
-        l2.push(*res.get(1).expect("Cannot get second element!"));
+        let mut res = line.split_whitespace()
+                .map(|val| val.parse::<i64>().expect("Not an int!"));
+        l1.push(res.next().expect("Cannot get first element!"));
+        l2.push(res.next().expect("Cannot get second element!"));
     }
     (l1, l2)
 }
