0f848dd74b7092bc7c7b79bd298993712badc5cc
LouisIsbister <louis.isbister@gmail.com>
2024-12-03 20:10:09 +1300

    Day 3 done

---
+++ day3/src/main.rs
@@ -0,0 +1,39 @@
+use regex::Regex;
+
+fn main() {
+    let file = std::fs::read_to_string("input.txt").expect("Cannot read file!");
+    let res = compute(&file);
+    
+    assert_eq!(59097164, res)
+}
+
+///
+/// Part 1 and 2
+/// Matches all the 'mul(?,?)'s, 'do()'s, and 'don't()'s, extracts the 
+/// values and adds them if the previous conditional capture was a 'do()'
+/// Finally returns the sum of the uncorrupted multiplications 
+/// 
+fn compute(file: &String) -> i64 {
+    let pattern = Regex::new(r"(mul\([0-9][0-9]{0,2},[0-9][0-9]{0,2}\)|don't\(\)|do\(\))").expect("Invalid regex");
+    let mut add = true;
+
+    let mut muls: Vec<(i64, i64)> = Vec::new();
+    for capture in pattern.find_iter(file) {
+        let cap = capture.as_str();
+        match cap {
+            "do()" => add = true, 
+            "don't()" => add = false,
+            _ => {
+                if add {
+                    let values: Vec<i64> = cap[4..cap.len() - 1].split(',') // remove leading 'mul(' and last ')' and split by comma
+                        .map(|e| e.parse().expect("Not an int!"))
+                        .collect();
+                    assert_eq!(2, values.len());
+                    muls.push((values[0], values[1]));
+                }
+            },
+        }
+    }
+
+    muls.iter().fold(0i64, |acc, e| acc + e.0 * e.1)
+}
